98d487fb757494cd7d1ac5c6088348aaf6c760b5
Kanishk <kanishka.ranjan.9@gmail.com>
2024-02-08 21:36:48 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState , useCallback } from 'react'
 import './App.css'
 
 function App() {
@@ -6,6 +6,17 @@ function App() {
   const [numberAllowed , setNumberAllowed] = useState(false);
   const [charAllowed , setCharAllowed] = useState(false);
   const [password , setPassword] = useState("");
+
+const passwordGenerator = useCallback(() => {
+  let pass = ""
+  let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+   if (numberAllowed) { str += "0123456789" }
+   if (charAllowed) { str += "!@#$%^&*(){}[]" }
+
+}, [length , numberAllowed , charAllowed , setPassword])
+
+}
+
 
   return (
     <>
